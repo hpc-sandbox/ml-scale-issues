e00b0b4ee8adef8c82c5592e64e9702b9286f3a3
aishwaryyasarkar <aishwaryya.sarkar@gmail.com>
2024-04-04 11:35:08 -0500

    add python and slurm script

---
+++ main.py
@@ -0,0 +1,57 @@
+import argparse
+from collections import defaultdict
+import socket
+import dgl
+import numpy as np
+import torch as th
+import tqdm
+import cProfile
+import sys, os
+import gc
+import datetime
+
+   
+def main(args):
+    """
+    Main function.
+    """
+    host_name = socket.gethostname()
+    print(f"{host_name}: Initializing DistDGL.")
+    dgl.distributed.initialize(args.ip_config)
+
+    print(f"{host_name}: Initializing PyTorch process group.")
+    th.distributed.init_process_group(backend=args.backend, timeout=datetime.timedelta(seconds=5400))
+    local_rank = args.local_rank
+
+    print(f"{host_name}: Initializing DistGraph.")
+    g = dgl.distributed.DistGraph(args.graph_name, part_config=args.part_config)
+    print(f"{host_name}: Finished loading graph {args.graph_name}.")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Distributed GraphSAGE.")
+    parser.add_argument("--graph_name", type=str, help="graph name")
+    parser.add_argument(
+        "--ip_config", type=str, help="The file for IP configuration"
+    )
+    parser.add_argument(
+        "--part_config", type=str, help="The path to the partition config file"
+    )
+    parser.add_argument(
+        "--backend",
+        type=str,
+        default="gloo",
+        help="pytorch distributed backend",
+    )
+    parser.add_argument(
+        "--num_gpus",
+        type=int,
+        default=0,
+        help="the number of GPU device. Use 0 for CPU training",
+    )
+
+    parser.add_argument(
+        "--local_rank", type=int, help="get rank of the process"
+    )
+
+    args = parser.parse_args()
+    main(args)
---
+++ run.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+#SBATCH -A m1302
+#SBATCH --nodes=1
+#SBATCH -q regular
+#SBATCH -t 00:30:00
+#SBATCH --constraint=cpu
+#SBATCH --job-name=ogbn-arxiv
+#SBATCH --output=ogbn-arxiv-%j.out
+#SBATCH --error=ogbn-arxiv-%j.err
+
+ulimit -c unlimited
+ulimit -v unlimited
+module load cudatoolkit/11.7
+source activate dgl-dev-gpu-117
+PYTHON_PATH=$(which python)
+
+DATASET_NAME="ogbn-arxiv"
+PARTITION_METHOD="metis"
+NUM_NODES=$SLURM_JOB_NUM_NODES
+SAMPLER_PROCESSES=0
+BACKEND="gloo"
+TRAINERS=4
+JOBID=$SLURM_JOB_ID
+
+DATA_DIR="/pscratch/sd/s/sark777/Distributed_DGL/dataset"
+PROJ_PATH=$(pwd)
+PARTITION_DIR="/pscratch/sd/s/sark777/Distributed_DGL/partitions/${PARTITION_METHOD}/${DATASET_NAME}/${NUM_NODES}_parts/${DATASET_NAME}.json"
+NODELIST=$(scontrol show hostnames $SLURM_JOB_NODELIST) # get list of nodes
+
+
+# append job id to IP_CONFIG_FILE
+IP_CONFIG_FILE="ip_config_${SLURM_JOB_ID}.txt"
+
+
+echo "Generating ip_config.txt..."
+: > $IP_CONFIG_FILE  # Empty the file if it exists
+
+for node in $NODELIST; do
+    echo "Getting first 10.249.x.x IP address for node: $node"
+
+    # Use srun to execute 'ip addr show' on each node and extract the first 10.249.x.x IP
+    first_ip=$(srun --nodes=1 --nodelist=$node ip addr show | grep 'inet 10.249' | awk '{print $2}' | cut -d'/' -f1 | head -n 1)
+
+    # Check if an IP address was found and append it to the ipconfig file
+    if [ ! -z "$first_ip" ]; then
+        echo $first_ip >> $IP_CONFIG_FILE
+    else
+        echo "No 10.249.x.x IP found for $node" >&2
+    fi
+done
+
+# Print the contents of the ipconfig file
+cat $IP_CONFIG_FILE
+
+# assert that the number of lines in ip_config.txt is equal to NUM_NODES
+NUM_IPS=$(wc -l < $IP_CONFIG_FILE)
+if [ "$NUM_IPS" -ne "$NUM_NODES" ]; then
+    echo "Number of IPs ($NUM_IPS) does not match number of nodes ($NUM_NODES)"
+    exit 1
+fi
+
+
+
+# Run the training script
+$PYTHON_PATH $PROJ_PATH/launch.py \
+--workspace $PROJ_PATH \
+--num_trainers $TRAINERS \
+--num_samplers $SAMPLER_PROCESSES \
+--num_servers 1 \
+--part_config $PARTITION_DIR \
+--ip_config  $IP_CONFIG_FILE \
+"$PYTHON_PATH main.py --graph_name $DATASET_NAME \
+--backend $BACKEND \
+--ip_config $IP_CONFIG_FILE"
